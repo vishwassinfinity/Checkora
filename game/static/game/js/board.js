97c85670eea1581c0017083e0702092dbf8396a0
Manuraj Goel <90852713+manurajgoel@users.noreply.github.com>
2026-05-22 15:57:32 +0530

    Correctly set offline status for status indicator
    Fix the status indicator to correctly show offline status.

--- game/static/game/js/board.js
+++ game/static/game/js/board.js
@@ -2183,7 +2183,7 @@ if (leaveConfirmNo) leaveConfirmNo.addEventListener('click', () => {
             const statusText = document.getElementById('status-text');
             function setOfflineStatus() {
             if (!statusIndicator || !statusText) return;
-                statusIndicator.classList.remove("offline");
+                statusIndicator.classList.add("offline"); #fixed
                 statusText.textContent = "offline";
             }
             function setOnlineStatus() {
